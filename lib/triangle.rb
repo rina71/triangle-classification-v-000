27392a9d8304fdee0790eeb6297db89f6ff195dc
Yurina Shin <yurinas@gmail.com>
2018-02-13 22:25:05 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def valid?
+    @a && @b && @c > 0 && (@a + @b > @c || @a + @c > @b || @b + @c > @a)
+  end
+
+  def kind
+  if valid?
+    if a == b && b == c
+      return :equilateral
+    elsif a == b || b == c || a == c
+      return :isosceles
+    else
+      return :scalene
+    end
+  elsif !(@a + @b > @c || @a + @c > @b || @b + @c > @a) && valid? == false
+    raise TriangleError
+    end
+  end
 end
+
+  class TriangleError < StandardError
+    "you are wrong"
+  end
